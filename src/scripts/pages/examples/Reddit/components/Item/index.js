f96b67b6a4a8551988a322ca84facc8019f616a6
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-25 02:53:05 +0300

    up

--- src/scripts/pages/examples/Reddit/components/Item/index.js
+++ src/scripts/pages/examples/Reddit/components/Item/index.js
@@ -1,5 +1,5 @@
 import ListItem from 'react-toolbox/lib/list/item';
 
 export default ({ title, score }) => {
-  return <ListItem rightIcon='message' caption={score.toString()} legend={title} />;
+  return <ListItem rightIcon='message' caption={`${score}`} legend={title} />;
 };
