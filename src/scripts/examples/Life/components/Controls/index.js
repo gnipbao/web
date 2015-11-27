adf46d1c1864c2d444c1b997621e4b29439154c4
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-27 21:32:27 +0300

    Lol

--- src/scripts/examples/Life/components/Controls/index.js
+++ src/scripts/examples/Life/components/Controls/index.js
@@ -1,12 +1,29 @@
 import Button from 'react-toolbox/lib/button';
 import style from './style';
 
-export default ({ iterate, clear, randomize }) => {
-  return (
-    <div className={style.root}>
-      <Button mini floating primary icon='cached' onClick={randomize} />
-      <Button mini floating icon='delete' onClick={clear} />
-      <Button floating accent icon='trending-flat' onClick={iterate} />
-    </div>
-  );
-};
+export default class Controls extends Component {
+  state = { interval: null };
+
+  play() {
+    const { iterate, speed } = this.props;
+    this.setState({ interval: setInterval(iterate, speed) });
+  }
+
+  stop() {
+    clearInterval(this.state.interval);
+    this.setState({ interval: null });
+  }
+
+  render() {
+    const { iterate, clear, randomize } = this.props;
+    return (
+      <div className={style.root}>
+        <Button mini floating primary icon='cached' onClick={randomize} />
+        <Button mini floating icon='delete' onClick={clear} />
+        <Button floating accent icon='skip-next' onClick={iterate} />
+        <Button floating icon='play-arrow' onClick={::this.play} />
+        <Button floating icon='stop' onClick={::this.stop} />
+      </div>
+    );
+  }
+}
