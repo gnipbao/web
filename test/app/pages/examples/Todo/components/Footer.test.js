606042763c7ca345276ab165433ae8dc02fb4dd9
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-12-09 21:42:17 +0300

    test(tests): Add test example

--- test/app/pages/examples/Todo/components/Footer.test.js
+++ test/app/pages/examples/Todo/components/Footer.test.js
@@ -8,4 +8,15 @@ describe('<Footer />', () => {
     const wrapper = shallow(<Footer />);
     expect(wrapper.find(Button)).to.have.length(2);
   });
+
+  it('correctly attaches click handlers', () => {
+    const completeAll = sinon.spy();
+    const clearCompleted = sinon.spy();
+
+    const wrapper = shallow(<Footer {...{ completeAll, clearCompleted } } />);
+    const buttons = wrapper.find(Button);
+    buttons.forEach(b => b.simulate('click'));
+
+    expect(completeAll.calledOnce).to.be.true;
+  });
 });
