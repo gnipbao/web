19f8d35a26780d3e3e54e0d8f9fdb83b44d947e7
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-12-16 14:49:00 +0300

    Don't apply any constraints to node when running in dev env

--- gulpfile.js/tasks/server.js
+++ gulpfile.js/tasks/server.js
@@ -4,10 +4,5 @@ const { watch } = config.app.argv;
 
 gulp.task('server', () => monitor('server', resolve.dist('server.js'), {
   watch,
-  killTree: true,
-  args: [
-    '--optimize_for_size',
-    '--max_old_space_size=460',
-    '--gc_interval=100'
-  ]
+  killTree: true
 }));
