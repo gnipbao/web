ebf002b5da1e09d72409f832d33f15e7cf3467c3
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-01-10 21:35:44 +0300

    feat(data): Add normalizr schemas

---
+++ src/scripts/api/schemas.js
@@ -0,0 +1,42 @@
+import { Schema, arrayOf } from 'normalizr';
+
+const room = new Schema('rooms');
+const roomArray = arrayOf(room);
+
+const playlist = new Schema('playlists');
+const playlistArray = arrayOf(playlist);
+
+const track = new Schema('tracks');
+const trackArray = arrayOf(track);
+
+const user = new Schema('users');
+const userArray = arrayOf(user);
+
+const provider = new Schema('providers');
+
+playlist.define({
+  owner: user,
+  provider
+});
+
+track.define({
+  owner: user,
+  provider
+});
+
+room.define({
+  tracks: arrayOf(track),
+  users: arrayOf(user),
+  owner: user,
+  playback: {
+    track
+  }
+});
+
+export {
+  room, roomArray,
+  playlist, playlistArray,
+  track, trackArray,
+  user, userArray,
+  provider
+}
