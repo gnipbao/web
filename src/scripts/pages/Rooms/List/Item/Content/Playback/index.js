dff155a042878e08d720d2b62dbddbb27b1df331
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-01-23 23:51:28 +0300

    fix: Fix styling issues

--- src/scripts/pages/Rooms/List/Item/Content/Playback/index.js
+++ src/scripts/pages/Rooms/List/Item/Content/Playback/index.js
@@ -1,4 +1,5 @@
 import css from 'react-css-modules';
+import { Link } from 'react-router';
 import Typist from 'react-typist';
 
 import format from 'lib/utils/format';
@@ -8,13 +9,17 @@ const { number, object } = PropTypes;
 const typistOptions = { cursor: { show: false } };
 
 export const Playback = (props) => {
-  const { track: { title, artist } } = props;
+  const { track: { id, title, artist } } = props;
   const formattedTitle = format(title);
   const formattedArtist = format(artist);
 
   const label = `${formattedTitle} by ${formattedArtist}`;
 
-  return <Typist styleName='root' {...typistOptions}>{label}</Typist>;
+  return (
+    <Typist {...typistOptions}>
+      <Link styleName='root' to={`/tracks/${id}`}>{label}</Link>
+    </Typist>
+  );
 };
 
 Playback.propTypes = {
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   "extends": "stylelint-config-suitcss",
   "rules": {
-    "rule-properties-order": [],
     "at-rule-empty-line-before": []
   }
 }
